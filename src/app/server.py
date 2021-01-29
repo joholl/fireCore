850b0711b8afe6a88b3911bc883d4327736e5bbd
Johannes Holland <joh.ho@gmx.de>
2021-01-29 13:11:24 +0000

    app: add logging

--- src/app/server.py
+++ src/app/server.py
@@ -1,15 +1,18 @@
 #!/usr/bin/env python3
 
 import contextlib
+import fastapi
+import logging
 import os
-import uvicorn
-
-from fastapi import FastAPI
 import threading
 import time
+import uvicorn
+
 from typing import Optional
 
-app = FastAPI()
+logger = logging.getLogger(__name__)
+
+app = fastapi.FastAPI()
 
 
 # TODO has to be called from ./src: python3 server.py
@@ -18,6 +21,11 @@ app = FastAPI()
 @app.get("/")
 def read_root():
     return {"Hello": "World"}
+
+
+@app.get("/log")
+def read_item():
+    return fastapi.responses.FileResponse('app.log')
 
 
 @app.get("/items/{item_id}")
@@ -43,12 +51,38 @@ class Server(uvicorn.Server):
 
 
 if __name__ == "__main__":
+    # setup logging
+    root_logger = logging.getLogger()
+    root_logger.setLevel(logging.NOTSET)
+
+    format_str = '%(asctime)s - %(name)s - %(levelname)s - %(message)s'
+    formatter = logging.Formatter(format_str)
+
+    stream_handler = logging.StreamHandler()
+    stream_handler.setLevel(logging.NOTSET)
+    stream_handler.setFormatter(formatter)
+    root_logger.addHandler(stream_handler)
+
+    # TODO /var/log/app.log
+    file_handler = logging.handlers.RotatingFileHandler('app.log', maxBytes=2000, backupCount=10)
+    file_handler.setLevel(logging.NOTSET)
+    file_handler.setFormatter(formatter)
+    root_logger.addHandler(file_handler)
+
+    # prepare logging for uvicorn
+    log_config = uvicorn.config.LOGGING_CONFIG
+    for k, v in log_config["loggers"].items():
+        log_config["loggers"][k]["handlers"] = []
+        log_config["loggers"][k]["level"] = logging.NOTSET
+        log_config["loggers"][k]["propagate"] = True
+
+    # setup http server
     module_name = os.path.splitext(__file__)[0]
-    config = uvicorn.Config(f"{module_name}:app", host="0.0.0.0", port=8000, reload=True, debug=True, workers=3, log_level="info")
+    config = uvicorn.Config(f"{module_name}:app", host="0.0.0.0", port=8000, reload=True, workers=3, log_config=log_config)
     server = Server(config=config)
 
     with server.run_in_thread():
         while True:
-            print("do stuff...")
+            root_logger.info("do stuff...")
             time.sleep(1)
 
