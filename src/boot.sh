a4e070d438995ed916aef339e817f7c5ee2da346
Johannes Holland <joh.ho@dmx.de>
2021-02-03 18:53:38 +0000

    Add PoC for changing hostname at boot

--- src/boot.sh
+++ src/boot.sh
@@ -3,6 +3,15 @@
 # change password of user 'tc' to 'piCore'
 sh -c "echo 'tc:piCore' | chpasswd"
 
+# change hostname and re-advertise
+# TODO start dhcp per init.d service (does not work unless interface is down?)
+/etc/init.d/services/dhcp stop
+sethostname bla
+NETDEVICES="$(awk -F: '/eth.:|tr.:/{print $1}' /proc/net/dev 2>/dev/null)"  # eth0
+for DEVICE in $NETDEVICES; do
+    /sbin/udhcpc -b -i $DEVICE -x hostname:$(/bin/hostname) -p /var/run/udhcpc.$DEVICE.pid >/dev/null 2>&1 &
+done
+
 # install python packages using wheel
 python3 -m pip install /tmp/wheel/optional/*
 
